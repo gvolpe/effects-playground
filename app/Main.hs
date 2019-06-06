be9f11955c82db81042ec968116c2389529e7bc2
Gabriel Volpe <volpegabriel@gmail.com>
2019-06-06 15:09:58 -0600

    RIO Monad effect

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,10 @@ import qualified Polysemy                      as P
 import           Control.Monad.IO.Class         ( liftIO )
 import           Fusion
 import           Poly                           ( echoIO )
+import           ReaderIO                       ( echoR
+                                                , Env(..)
+                                                )
+import           RIO                            ( runRIO )
 
 fusedMain :: IO ()
 fusedMain = F.runM $ runTeletypeIO fusedEcho
@@ -14,5 +18,8 @@ fusedMain = F.runM $ runTeletypeIO fusedEcho
 polyMain :: IO ()
 polyMain = P.runM echoIO
 
+rioMain :: IO ()
+rioMain = runRIO (Env "123") echoR
+
 main :: IO ()
-main = fusedMain
+main = rioMain
---
+++ src/ReaderIO.hs
@@ -0,0 +1,27 @@
+module ReaderIO where
+
+import           Control.Monad.Reader           ( ask )
+import           Data.Monoid                    ( (<>) )
+import           RIO                     hiding ( traceId )
+
+newtype Env = Env { traceId :: String } deriving (Show)
+
+tracedRead :: RIO Env String
+tracedRead = do
+  env <- ask
+  liftIO $ putStrLn $ ">>> Read -> Trace-Id: " <> traceId env
+  liftIO getLine
+
+tracedWrite :: String -> RIO Env ()
+tracedWrite i = do
+  env <- ask
+  liftIO $ putStrLn $ ">>> Write -> Trace-Id: " <> traceId env
+  liftIO $ putStrLn i
+
+echoR :: RIO Env ()
+echoR = do
+  i <- tracedRead
+  case i of
+    "" -> pure ()
+    _  -> tracedWrite i >> echoR
+
