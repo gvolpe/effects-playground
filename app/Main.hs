c1cb4b845b59a1e89bc2e5ffe1050b7e7e51895b
Gabriel Volpe <volpegabriel@gmail.com>
2019-06-05 21:42:03 -0600

    polysemy example

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,19 @@ import           Fusion                         ( runTeletypeRet
                                                 , read
                                                 , write
                                                 )
+import           Poly                           ( echoIO )
+import           Polysemy                       ( runM )
 
-main :: IO ()
-main = do
+fusedMain :: IO ()
+fusedMain = do
   putStrLn "Hello, Haskell!"
   print $ run (runTeletypeRet ["input"] read)                        -- single input
   print $ run (runTeletypeRet ["input"] (write "output"))            -- single output
   print $ run (runTeletypeRet ["in"] (write "out1" >> write "out2")) -- multiple outputs
+
+-- echo forever
+polyMain :: IO ()
+polyMain = runM echoIO
+
+main :: IO ()
+main = polyMain
---
+++ src/Poly.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE LambdaCase, BlockArguments #-}
+{-# LANGUAGE GADTs, FlexibleContexts, TypeOperators, DataKinds, PolyKinds #-}
+
+module Poly
+  ( echoIO
+  )
+where
+
+import           Polysemy
+import           Polysemy.Input
+import           Polysemy.Output
+
+data Teletype m a where
+  ReadTTY  ::Teletype m String
+  WriteTTY ::String -> Teletype m ()
+
+makeSem ''Teletype
+
+runTeletypeIO :: Member (Lift IO) r => Sem (Teletype ': r) a -> Sem r a
+runTeletypeIO = interpret $ \case
+  ReadTTY      -> sendM getLine
+  WriteTTY msg -> sendM $ putStrLn msg
+
+echo :: Member Teletype r => Sem r ()
+echo = do
+  i <- readTTY
+  case i of
+    "" -> pure ()
+    _  -> writeTTY i >> echo
+
+echoIO :: Sem '[Lift IO] ()
+echoIO = runTeletypeIO echo
+
